2838ad591065e27b17b4d1c8807e13aee2db6c11
Michael Curry <mike@vuurr.com>
2013-02-27 20:17:54 -0700

    -> Vuurr's _straps branding

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
  *
  * Displays all of the <head> section and everything up till <div id="main">
  *
- * @package _s
- * @since _s 1.0
+ * @package _straps
+ * @since _straps 1.0
  */
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
@@ -32,8 +32,8 @@
 		</hgroup>
 
 		<nav id="site-navigation" class="navigation-main" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></h1>
-			<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', '_s' ); ?></a></div>
+			<h1 class="menu-toggle"><?php _e( 'Menu', '_straps' ); ?></h1>
+			<div class="assistive-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_straps' ); ?>"><?php _e( 'Skip to content', '_straps' ); ?></a></div>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- .site-navigation .main-navigation -->
--- page.php
+++ page.php
@@ -7,8 +7,8 @@
  * and that other 'pages' on your WordPress site will use a
  * different template.
  *
- * @package _s
- * @since _s 1.0
+ * @package _straps
+ * @since _straps 1.0
  */
 
 get_header(); ?>
