74b11e4d934e392fa9dab49cbf443fb662f0a67c
chuckreynolds <chuck@vuurr.com>
2013-05-18 16:33:14 -0700

    rem uneeded in/out php

--- comments.php
+++ comments.php
@@ -10,16 +10,14 @@
  * @package _straps
  * @since _straps 1.0
  */
-?>
 
-<?php
-	/*
-	 * If the current post is protected by a password and
-	 * the visitor has not yet entered the password we will
-	 * return early without loading the comments.
-	 */
-	if ( post_password_required() )
-		return;
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() )
+	return;
 ?>
 
 	<div id="comments" class="comments-area">
