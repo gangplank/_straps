405ab7896d338cafbc3b971f9a21096d1192d307
chuckreynolds <chuck@vuurr.com>
2013-05-18 16:53:13 -0700

    404 Only show the widget if site has multiple categories

--- 404.php
+++ 404.php
@@ -23,12 +23,22 @@ get_header(); ?>
 
 					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
+					<?php if ( _s_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 					<div class="widget">
 						<h2 class="widgettitle"><?php _e( 'Most Used Categories', '_straps' ); ?></h2>
 						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
+						<?php
+							wp_list_categories( array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							) );
+						?>
 						</ul>
 					</div><!-- .widget -->
+					<?php endif; ?>
 
 					<?php
 					/* translators: %1$s: smiley */
