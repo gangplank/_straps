653f78fb88787426028b3d6823c2c4d33716d187
Jade Meskill <jade@integrumtech.com>
2013-09-13 20:25:29 -0700

    forgot container for nav

--- header.php
+++ header.php
@@ -25,27 +25,29 @@
 <body <?php body_class(); ?>>
 
 <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="#">_straps</a>
-  </div>
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">_straps</a>
+    </div>
 
-  <div class="collapse navbar-collapse navbar-ex1-collapse">
-    <ul class="nav navbar-nav">
-      <?php
-        $args['container'] = '';
-        $args['container_class'] = '';
-        $args['menu_class'] = 'main-menu';
-        $args['items_wrap'] = '%3$s';
-        $args['walker'] = new menu_walker();
-        wp_nav_menu($args)
-      ?>
-    </ul>
-  </div><!--/.navbar-collapse -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+      <ul class="nav navbar-nav">
+        <?php
+          $args['container'] = '';
+          $args['container_class'] = '';
+          $args['menu_class'] = 'main-menu';
+          $args['items_wrap'] = '%3$s';
+          $args['walker'] = new menu_walker();
+          wp_nav_menu($args)
+        ?>
+      </ul>
+    </div><!--/.navbar-collapse -->
+  </div><!--/.container -->
 </nav>
 <div class="container">
